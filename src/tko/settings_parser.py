87178ca2199af623c14daa5b971d1d982ca810a5
lion <sena.ufc@gmail.com>
2023-08-05 07:27:46 -0300

    test_data

--- src/tko/settings_parser.py
+++ src/tko/settings_parser.py
@@ -6,7 +6,9 @@ import appdirs
 
 class SettingsParser:
     package_name = "tko"
-    settings_file = os.path.join(user_data_dir(package_name), "settings.cfg")
+    file_name = "settings.cfg"
+
+    settings_file = os.path.join(user_data_dir(package_name), file_name)
 
     default_cfg_content = """[REP]
 fup = https://raw.githubusercontent.com/qxcodefup/arcade/master/base/
@@ -16,11 +18,6 @@ poo = https://raw.githubusercontent.com/qxcodepoo/arcade/master/base/
 
     @staticmethod
     def get_repository(disc):
-        print("user_data_dir" + appdirs.user_data_dir(SettingsParser.package_name))
-        print("user_config_dir" + appdirs.user_config_dir(SettingsParser.package_name))
-        print("user_cache_dir" + appdirs.user_cache_dir(SettingsParser.package_name))
-        print("site_data_dir" + appdirs.site_data_dir(SettingsParser.package_name))
-
         if not os.path.isfile(SettingsParser.settings_file):
             SettingsParser.create_default_settings_file()
         with open(SettingsParser.settings_file, "r") as f:
@@ -28,9 +25,9 @@ poo = https://raw.githubusercontent.com/qxcodepoo/arcade/master/base/
             parser.read(SettingsParser.settings_file)
             return parser["REP"][disc]
 
-    @staticmethod
-    def create_default_settings_file():
-        if not os.path.isdir(user_data_dir(SettingsParser.package_name)):
-            os.mkdir(user_data_dir(SettingsParser.package_name))
-        with open(SettingsParser.settings_file, "w") as f:
-            f.write(SettingsParser.default_cfg_content)
+    # @staticmethod
+    # def create_default_settings_file():
+    #     if not os.path.isdir(user_data_dir(SettingsParser.package_name)):
+    #         os.mkdir(user_data_dir(SettingsParser.package_name))
+    #     with open(SettingsParser.settings_file, "w") as f:
+    #         f.write(SettingsParser.default_cfg_content)
