ea2548de94458698497198278435c7cd3e740259
lion <sena.ufc@gmail.com>
2023-08-06 13:32:19 -0300

    replit cpp

--- replit/merge.py
+++ replit/merge.py
@@ -34,9 +34,9 @@ for file in files:
     with open("../src/tko/" + file + ".py", "r") as f:
         lines = f.read().split("\n")
         for line in lines:
-            if line.startswith("from ."):
+            if line.startswith("from .") or line.startswith("from __future__"):
                 pass
-            elif line.startswith("import"):
+            elif line.startswith("import") or line.startswith("from "):
                 if line not in imports:
                     imports.append(line)
             else:
@@ -45,6 +45,7 @@ for file in files:
 with open("tko", "w") as f:
     f.write("#!/usr/bin/env python3\n")
     f.write("# -*- coding: utf-8 -*-\n")
+    f.write("from __future__ import annotations\n\n")
     f.write("\n".join(imports))
     f.write("\n")
     f.write("\n".join(output))
