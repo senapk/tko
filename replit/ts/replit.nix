ad0e776421ce9d6132a6e42f4b0fe2270eebf6ae
lion <sena.ufc@gmail.com>
2023-08-07 10:45:49 -0300

    replit js support

---
+++ replit/ts/replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+	deps = [
+    pkgs.python38Full
+    pkgs.python38Packages.appdirs
+    pkgs.nodejs-18_x
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.replitPackages.jest
+	];
+}
---
+++ replit/ts/update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+REP="${HOME}/${REPL_SLUG}"
+mkdir -p ${REP}/.bin
+
+SOURCE="https://raw.githubusercontent.com/senapk/tko/master/replit/tko"
+TARGET="${REP}/.bin/tko"
+curl  ${SOURCE} -o ${TARGET}
+chmod +x ${TARGET}
+
+SOURCE="https://raw.githubusercontent.com/senapk/tko/master/replit/bashrc"
+TARGET="${REP}/.bashrc"
+curl  ${SOURCE} -o ${TARGET}
+
+SOURCE="https://raw.githubusercontent.com/senapk/tko/master/replit/js/update.sh"
+TARGET="${REP}/.bin/update.sh"
+curl  ${SOURCE} -o ${TARGET}
+chmod +x ${TARGET}
+
+SOURCE="https://raw.githubusercontent.com/senapk/tko/master/replit/js/replit"
+TARGET="${REP}/.replit"
+curl  ${SOURCE} -o ${TARGET}
+
+SOURCE="https://raw.githubusercontent.com/senapk/tko/master/replit/js/replit.nix"
+TARGET="${REP}/replit.nix"
+curl  ${SOURCE} -o ${TARGET}
+
+
