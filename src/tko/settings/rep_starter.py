6482378c2b7ad28c68d864c1c2f9d38d1aab427f
sena <sena.ufc@gmail.com>
2026-04-07 14:09:02 -0300

    fix: tko init error

--- src/tko/settings/rep_starter.py
+++ src/tko/settings/rep_starter.py
@@ -72,6 +72,6 @@ class RepStarter:
     def create_empty_rep(self):
         source = self.repo.get_student_sandbox()
         self.repo.data.set_source(source)
-        folder = source.get_origin()
+        folder = source.get_workspace()
         print(f"Criando repositório vazio, utilizando a pasta {folder} como pasta para atividades locais")
     
