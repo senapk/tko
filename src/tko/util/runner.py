dc56c25e8bf77b1372563c69c6a3802b6f618f20
David Sena <sena.ufc@gmail.com>
2026-05-27 09:49:02 -0300

    fix: windows att7

--- src/tko/util/runner.py
+++ src/tko/util/runner.py
@@ -10,6 +10,12 @@ class Runner:
 
     @staticmethod
     def subprocess_run(cmd: str | list[str], input_data: str = "", timeout: None | float = None, folder: Path | None = None) -> tuple[int, str, str]:
+        if os.name == 'nt':
+            shell = True
+        else:
+            shell = isinstance(cmd, str)
+        
+        
         try:
             env = os.environ.copy()
             env['NO_COLOR'] = '1'
@@ -21,6 +27,7 @@ class Runner:
                                     stdout=PIPE, 
                                     stderr=PIPE, 
                                     timeout=timeout, 
+                                    shell=shell,
                                     text=True
                                     )
 
@@ -28,6 +35,9 @@ class Runner:
             if answer.returncode != 0:
                 err = Runner.decode_code(answer.returncode)
             return answer.returncode, answer.stdout, err
+        except FileNotFoundError:
+            err = "fail: comando não encontrado ({})".format(cmd if isinstance(cmd, str) else cmd[0])
+            return 1, "", err
         except subprocess.TimeoutExpired:
             err = "fail: processo abortado depois de {} segundos".format(timeout)
             return 1, "", err
